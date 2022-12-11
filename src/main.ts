1adda468bd19a65de566975d1ed8e0476a008036
Nikita <script696@yandex.ru>
2022-12-11 11:03:23 +0300

    feat: add project skeleton

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-import {NestFactory} from "@nestjs/core";const start = async () => {    try {        const app = await NestFactory.create()    } catch {        console.log(e)    }}
+import {NestFactory} from "@nestjs/core";import {AppModule} from "./app.module";const start = async () => {    try {        const PORT = process.env.PORT || 5000        const app = await NestFactory.create(AppModule)        await app.listen(PORT, ()=> console.log(`serbver start on PORT ${PORT}`))    } catch(e) {        console.log(e)    }}start()
---
+++ src/track/track.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('/tracks')
+export class TrackController {
+  create() {}
+
+  @Get()
+  getAll() {
+    return 'Work';
+  }
+  getOne() {}
+  delete() {}
+}
