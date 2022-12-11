a0aac0598d89d1c2bbf8db6a7d77fb740827c959
Nikita <script696@yandex.ru>
2022-12-11 22:34:04 +0300

    feat: add file static resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { FileModule } from './file/file.module';
+import * as path from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
     MongooseModule.forRoot('mongodb://localhost:27017/script-api'),
     TrackModule,
     FileModule,
